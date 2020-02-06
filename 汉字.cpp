da59b303d9aabb3d67ee4e2197fbe193cac4dc20
Hongchenglong <35827686+Hongchenglong@users.noreply.github.com>
2020-02-06 19:08:41 +0800

    version

--- 汉字.cpp
+++ 汉字.cpp
@@ -18,24 +18,22 @@ int toint(string value) { // toint函数将 汉字 转成 整数
 	return ret;
 }
 int main() {
-	string var, value;
+	map<string, int> mp; 
+	string var, value, some, oper, cmp, other;
 	string num[12] = {"零", "一", "二", "三", "四", "五", "六", "七", "八", "九", "十"};
 	for (int i = 0; i < 2; ++i) cin >> var; // 输入两遍，保留第二个变量名 
 	for (int i = 0; i < 2; ++i) cin >> value; // 输入两遍，保留第二个数字值 
-	int sum = toint(value); // 将value转换成整数 
-	string some, oper, cmp;
-	while (cin >> some) { // 输入一个单词，判断是否等于变量名或“看看”或“如果” 
-		if (some == var) {
-			cin >> oper >> value;
-			if (oper == "减少") {
-				sum -= toint(value); 
-			} else if (oper == "增加") {
-				sum += toint(value); 
-			} 
+	mp[var] = toint(value); // 将value转换成整数, 并存入mp[var]中 
+	
+	while (cin >> some) { // 输入一个单词，判断是否等于“整数”或“看看”或“如果”或变量名 
+		if (some == "整数") {
+			cin >> var >> other >> value; // 保留var和value 
+			mp[var] = toint(value);
 		} else if (some == "看看") {
 			cin >> var;
-			int temp = abs(sum);
-			if (sum < 0) cout << "负"; 
+			if (!mp[var]) continue; // 如果变量名不存在，则跳过 
+			int temp = abs(mp[var]);
+			if (mp[var] < 0) cout << "负"; 
 			if (temp <= 10) {
 				cout << num[temp] << endl;	
 			} else { // 大于10的情况 
@@ -52,18 +50,45 @@ int main() {
 				cout << endl;
 			}
 		} else if (some == "如果") {
-			string digit, sent1, sent2, other;
-			cin >> other >> cmp >> digit >> other;
-			cin >> other >> sent1 >> other >> other >> sent2;
-			int len1 = sent1.size()-2; // 减去头尾两个双引号的长度 
-			int len2 = sent2.size()-2;
-			if (cmp == "大于") {
-				if (sum > toint(digit)) cout << sent1.substr(1, len1) << endl;
-				else cout << sent2.substr(1, len2) << endl; // 输出去掉头尾的子串 
-			} else if (cmp == "小于") {
-				if (sum < toint(digit)) cout << sent1.substr(1, len1) << endl;
-				else cout << sent2.substr(1, len2) << endl;
-			}  
+			string digit, then, s1, s2, s3, s4, s5;
+			cin >> var >> cmp >> digit;
+			cin >> then >> s1 >> s2 >> s3 >> s4 >> s5;
+			if (s1 == "看看") {
+				int len1 = s2.size()-2; // 减去头尾两个双引号的长度 
+				int len2 = s5.size()-2;
+				if (cmp == "大于") {
+					if (mp[var] > toint(digit)) cout << s2.substr(1, len1) << endl;
+					else cout << s5.substr(1, len2) << endl; // 输出去掉头尾的子串 
+				} else if (cmp == "小于") {
+					if (mp[var] < toint(digit)) cout << s2.substr(1, len1) << endl;
+					else cout << s5.substr(1, len2) << endl;
+				} 
+			} else {
+				if (cmp == "大于") {
+					if (mp[var] > toint(digit)) {
+						if (s2 == "减少") {
+							mp[s1] -= toint(s3); 
+						} else if (s2 == "增加") {
+							mp[s1] += toint(s3); 
+						} 
+					} else; 
+				} else if (cmp == "小于") {
+					if (mp[var] < toint(digit)) {
+						if (s2 == "减少") {
+							mp[s1] -= toint(s3); 
+						} else if (s2 == "增加") {
+							mp[s1] += toint(s3); 
+						} 
+					} else;
+				} 
+			}		 
+		} else if (mp[some]) { // 如果是已存在的变量 
+			cin >> oper >> value;
+			if (oper == "减少") {
+				mp[some] -= toint(value); 
+			} else if (oper == "增加") {
+				mp[some] += toint(value); 
+			} 
 		} 
 	}
 	return 0;
